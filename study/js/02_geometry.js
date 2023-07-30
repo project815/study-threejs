9e65d002745231574ee8776e041cd074d5ce72b9
songminseok <ske3056@gmail.com>
2023-07-30 22:32:02 +0900

    curve

--- study/js/02_geometry.js
+++ study/js/02_geometry.js
@@ -35,7 +35,7 @@ class App {
 		const width = this._divContainer.clientWidth;
 		const height = this._divContainer.clientHeight;
 		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-		camera.position.z = 2;
+		camera.position.z = 15;
 		this._camera = camera;
 	}
 	_setupLight() {
@@ -45,7 +45,7 @@ class App {
 		light.position.set(-1, 2, 4);
 		this._scene.add(light);
 	}
-
+	/* 기본 geometry*/
 	// _setupModel() {
 	// 	//BoxGeometry
 	// 	// const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -123,44 +123,73 @@ class App {
 	// 	this._scene.add(group);
 	// 	this._cube = group;
 	// }
-	_setupModel() {
-		const shape = new THREE.Shape();
-		const x = -2.5,
-			y = -2.5;
-		shape.moveTo(x + 2.5, y + 2.5);
-		shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
-		shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
-		shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
-		shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
-		shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
-		shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+	/* custom geometry : square, heart*/
+	// _setupModel() {
+	// 	const shape = new THREE.Shape();
+	// 	const x = -2.5,
+	// 		y = -2.5;
+	// 	shape.moveTo(x + 2.5, y + 2.5);
+	// 	shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+	// 	shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+	// 	shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+	// 	shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+	// 	shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+	// 	shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
 
-		// shape.moveTo(1, 1);
-		// shape.lineTo(1, -1);
-		// shape.lineTo(-1, -1);
-		// shape.lineTo(-1, 1);
-		// shape.closePath();
+	// 	// shape.moveTo(1, 1);
+	// 	// shape.lineTo(1, -1);
+	// 	// shape.lineTo(-1, -1);
+	// 	// shape.lineTo(-1, 1);
+	// 	// shape.closePath();
 
-		const geometry = new THREE.ShapeGeometry(shape);
+	// 	const geometry = new THREE.ShapeGeometry(shape);
 
-		//지리정보 시스템, GIS에서 사용됨. 3차원 지형표현에 유용하게 사용되어짐.
-		const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
-		const cube = new THREE.Mesh(geometry, fillMaterial);
+	// 	//지리정보 시스템, GIS에서 사용됨. 3차원 지형표현에 유용하게 사용되어짐.
+	// 	const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
+	// 	const cube = new THREE.Mesh(geometry, fillMaterial);
 
-		const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
-		const line = new THREE.LineSegments(
-			new THREE.WireframeGeometry(geometry),
-			//WireframeGeometry 모든 외곽선이
-			lineMaterial
-		);
+	// 	const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
+	// 	const line = new THREE.LineSegments(
+	// 		new THREE.WireframeGeometry(geometry),
+	// 		//WireframeGeometry 모든 외곽선이
+	// 		lineMaterial
+	// 	);
 
-		const group = new THREE.Group();
-		group.add(cube);
-		group.add(line);
+	// 	const group = new THREE.Group();
+	// 	group.add(cube);
+	// 	group.add(line);
 
-		this._scene.add(group);
-		this._cube = group;
-	}
+	// 	this._scene.add(group);
+	// 	this._cube = group;
+	// }
+	/* custom geometry : curve*/
+	// _setupModel() {
+	// 	class CustomSinCurve extends THREE.Curve {
+	// 		constructor(scale) {
+	// 			super();
+	// 			this.scale = scale;
+	// 		}
+	// 		getPoint(t) {
+	// 			const tx = t * 3 - 1.5;
+	// 			const ty = Math.sin(2 * Math.PI * t);
+	// 			const tz = 0;
+	// 			return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+	// 		}
+	// 	}
+
+	// 	const path = new CustomSinCurve(4);
+
+	// 	const geometry = new THREE.BufferGeometry();
+	// 	const points = path.getPoints(6);
+	// 	geometry.setFromPoints(points);
+
+	// 	const material = new THREE.LineBasicMaterial({ color: 0xffff00 });
+	// 	const line = new THREE.Line(geometry, material);
+
+	// 	this._scene.add(line);
+	// }
+	/* custom geometry : curve mesh*/
+	_setupModel() {}
 
 	onResize() {
 		const width = this._divContainer.clientWidth;
